eb358411bbb0baba665b5e52d88e88ec4e431ade
Rhythm1821 <alamrawat1821@gmail.com>
2024-06-29 14:09:27 +0530

    warning fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -132,9 +132,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
